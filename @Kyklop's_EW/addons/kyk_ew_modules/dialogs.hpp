f7d3c27db7adb0123a53ca11a977b5c3744b0bfb
KyklopCZ <68820136+KyklopCZ@users.noreply.github.com>
2020-08-01 11:45:15 +0200

    Added Zeus modules

---
+++ @Kyklop's_EW/addons/kyk_ew_modules/dialogs.hpp
@@ -0,0 +1,21 @@
+class kyk_ew_modules_broadcastJammerDialog
+{
+	idd = 5870;
+	movingEnabled = true;
+	
+	class controls
+	{
+		#include "GUIs\broadcastJammerAddGUI.hpp"
+	};
+};
+
+class kyk_ew_modules_directedJammerDialog
+{
+	idd = 5871;
+	movingEnabled = true;
+	
+	class controls
+	{
+		#include "GUIs\directedJammerAddGUI.hpp"
+	};
+};
---
+++ @Kyklop's_EW/addons/kyk_ew_modules/scripts/fn_broadcastJammerAdd.sqf
@@ -0,0 +1,3 @@
+createDialog "kyk_ew_modules_broadcastJammerDialog";
+
+waitUntil {!isNull (findDisplay 5870);};	//Wait until the dialog is created
